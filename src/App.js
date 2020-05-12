da0d4e26a548222ba03d7907c1f206237d09a339
Mehmet COŞKUN <mehmet@netrass.com>
2020-05-12 19:03:07 +0300

    updating

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import loop from './assets/audios/loop.mp3';
-import win from './assets/audios/win.mp3';
+import loopAudio from './assets/audios/loop.mp3';
+import winAudio from './assets/audios/win.mp3';
 import './assets/css/App.css';
 
 class App extends Component {
@@ -11,10 +11,14 @@ class App extends Component {
 
   showFile = async e => {
     e.preventDefault();
+    if (e.target.files[0].name.split('.').pop().toLowerCase() !== 'txt') {
+      alert('Çekiliş listesi sadece Metin Belgesi (txt) olabilir!');
+      return false;
+    }
     const reader = new FileReader();
     reader.onload = async e => {
-      const text = (e.target.result);
-      const names = text.replace(/\r\n/g, "\n").split("\n").filter(line => line);
+      const text = e.target.result;
+      const names = text.replace(/\r\n/g, '\n').split('\n').filter(line => line);
       this.setState({
         names
       });
@@ -22,48 +26,45 @@ class App extends Component {
     reader.readAsText(e.target.files[0]);
   }
 
-  onSelectFileClick = () => {
-    document.getElementById("file").click();
-  }
-
   onClick = () => {
+    const loopAudio = document.getElementsByClassName('loopAudio')[0];
+    const winAudio = document.getElementsByClassName('winAudio')[0];
+    loopAudio.play();
     let i = 0;
-    const loop = document.getElementsByClassName("loop")[0];
-    const win = document.getElementsByClassName("win")[0];
-    loop.play();
-    const random = setInterval(() => {
-      document.getElementById("headerNames").innerHTML = this.state.names[i++ % this.state.names.length];
+    const randomNames = setInterval(() => {
+      document.getElementById('names').innerHTML = this.state.names[i++ % this.state.names.length];
     }, 50);
     setTimeout(() => {
-      clearInterval(random);
-      loop.pause();
-      win.play();
+      clearInterval(randomNames);
+      loopAudio.pause();
+      winAudio.play();
       this.setState({
         name: this.state.names[Math.floor(Math.random() * (this.state.names.length))]
       });
     }, 5000);
   }
 
-  reset = () => {
-    window.location.reload();
-  }
   render() {
     return (
       <div>
-        <audio className="loop"><source src={loop}></source></audio>
-        <audio className="win"><source src={win}></source></audio>
-        <h1 id="headerNames">{this.state.name}</h1>
+        <audio className="loopAudio"><source src={loopAudio}></source></audio>
+        <audio className="winAudio"><source src={winAudio}></source></audio>
+        <h1 id="names">{this.state.name}</h1>
         <div className="wrap">
           {
             !this.state.names.length ? (
               <div>
-                <button className="selectFile" onClick={this.onSelectFileClick}>Çekiliş Listesini Aktar (txt)</button>
+                <button className="selectFile" onClick={() => {
+                  document.getElementById('file').click();
+                }}>Çekiliş Listesini Aktar (txt)</button>
                 <input type="file" id="file" onChange={e => this.showFile(e)} />
               </div>
-            ) : this.state.name === "?" ? (
+            ) : this.state.name === '?' ? (
               <button className="start" onClick={this.onClick}>Şanslı kişiyi seç</button>
             ) : (
-                  <button className="reset" onClick={this.reset}>Sıfırla</button>
+                  <button className="reset" onClick={() => {
+                    window.location.reload();
+                  }}>Sıfırla</button>
                 )
           }
         </div>
